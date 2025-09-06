54a5ef606e587a3e0708e121f5d47cc0ca6a894b
chenyan86919411 <chenyan86919411@gmail.com>
2025-09-06 12:12:10 +0800

    coze web 对外开放

--- backend/build.sh
+++ backend/build.sh
@@ -3,4 +3,4 @@ RUN_NAME=hertz_service
 mkdir -p output/bin
 cp script/* output 2>/dev/null
 chmod +x output/bootstrap.sh
-go build --ldflags="-checklinkname=0" -o output/bin/${RUN_NAME}
+go build -o output/bin/${RUN_NAME}
